3dffb3ff4d1307d9aca9dfb112d8b500fee7d940
utku-bilgin <h.utkubilgin@gmail.com>
2024-02-11 19:46:28 +0300

    Temel entity ler oluşturuldu

---
+++ Core/ShoppingApi.Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using ShoppingApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/ShoppingApi.Domain/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
